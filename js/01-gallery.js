c7da121688cf983d39244ea72c7414bcf51b524b
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-21 19:43:31 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const fragment = document.createDocumentFragment();
+
+for (var i = 0; i < galleryItems.length; i += 1) {
+  const listItem = document.createElement("li");
+  listItem.className = "gallery__item";
+
+  const link = document.createElement("a");
+  link.className = "gallery__link";
+  link.href = galleryItems[i].original;
+
+  const image = document.createElement("img");
+  image.className = "gallery__image";
+  image.src = galleryItems[i].preview;
+  image.setAttribute("data-source", `${galleryItems[i].original}`);
+  image.alt = galleryItems[i].description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  fragment.appendChild(listItem);
+}
+
+// Додавання фрагмента в DOM
+ulEl.appendChild(fragment);
+
+ulEl.addEventListener("click", onClickImg);
+
+function onClickImg(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}">
+  `);
+
+  instance.show();
+}

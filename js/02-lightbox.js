eee34d63f712804e196424156211a9a28ce828ed
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-21 20:55:45 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const fragment = document.createDocumentFragment();
+
+for (var i = 0; i < galleryItems.length; i += 1) {
+  const listItem = document.createElement("li");
+  listItem.className = "gallery__item";
+
+  const link = document.createElement("a");
+  link.className = "gallery__link";
+  link.href = galleryItems[i].original;
+
+  const image = document.createElement("img");
+  image.className = "gallery__image";
+  image.src = galleryItems[i].preview;
+  image.alt = galleryItems[i].description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  fragment.appendChild(listItem);
+}
+
+// Додавання фрагмента в DOM
+ulEl.appendChild(fragment);
+
+ulEl.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "Alt",
+  });
+}

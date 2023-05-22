26370016da7d3c749d97a54a5f32788f807a52f6
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-22 22:46:07 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,46 +2,30 @@ import { galleryItems } from "./gallery-items.js";
 
 const ulEl = document.querySelector(".gallery");
 
-const fragment = document.createDocumentFragment();
-
-for (var i = 0; i < galleryItems.length; i += 1) {
-  const listItem = document.createElement("li");
-  listItem.className = "gallery__item";
-
-  const link = document.createElement("a");
-  link.className = "gallery__link";
-  link.href = galleryItems[i].original;
-
-  const image = document.createElement("img");
-  image.className = "gallery__image";
-  image.src = galleryItems[i].preview;
-  image.setAttribute("data-source", `${galleryItems[i].original}`);
-  image.alt = galleryItems[i].description;
-
-  link.appendChild(image);
-  listItem.appendChild(link);
-
-  fragment.appendChild(listItem);
-}
-
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li >`;
+});
 // Додавання фрагмента в DOM
-ulEl.appendChild(fragment);
-
+ulEl.insertAdjacentHTML("afterbegin", markup.join(""));
 ulEl.addEventListener("click", openModal);
 
 function openModal(event) {
   event.preventDefault();
-
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
   const instance = basicLightbox.create(`
       <img src="${event.target.dataset.source}">
   `);
 
   instance.show();
 }
-
-// const closeModalOnEscape = (event) => {
-//   if (event.key === "Escape") {
-//     basicLightbox.close();
-//   }
-// };
-// document.addEventListener("keydown", closeModalOnEscape);
